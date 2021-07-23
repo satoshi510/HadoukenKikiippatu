c2d71d09e42aef361d13dedb2daa78da29e4216e
satoshi <sato-naka@kfy.biglobe.ne.jp>
2021-07-23 18:37:20 +0900

    消し関数を導入
    消し関数をまとめてスクリプト短縮
    スクリプト整理

--- HadoukennKikiippatu/Assets/PaneruController.cs
+++ HadoukennKikiippatu/Assets/PaneruController.cs
@@ -114,7 +114,6 @@ public class PaneruController : MonoBehaviour
             {
                 ColorChange(Buttons[x,y], Paneru[x,y]);
             }
-
         }
         KesikataColorChange(JyuujikesiButton, isJyuujikesiButton);
         KesikataColorChange(KurosukesiButton, isKurosukesiButton);
@@ -179,6 +178,7 @@ public class PaneruController : MonoBehaviour
         toikata = Random.Range(1, 4);
         //問題マス数 (3～5マス)
         toisum = Random.Range(3, 6);
+        //現状のtoiのマスの合計の計算用
         int toisumnow = 0;
         //toi初期化
         for (int Seti = 0; Seti < toi.GetLength(0); Seti++)
@@ -202,7 +202,7 @@ public class PaneruController : MonoBehaviour
             {
                 Toisakuseikurosu(Setx);
             }
-            else if (toikata ==3)
+            else if (toikata == 3)
             {
                 Toisakuseirasen(Setx);
             }
@@ -422,78 +422,32 @@ public class PaneruController : MonoBehaviour
         }
 
     }
-    //十字消し関数
-    void Jyuujikesi(int Jkesix, int Jkesiy)
-    {
-        int Jkesik = 0;
-        int Jkesil = 0;
 
-        for (int Jkesii = Jkesix - 2; Jkesii < Jkesix + 3; Jkesii++)
+    //消し関数
+    void Kesi(int x, int y, int[,] kesi)
+    {
+
+        int k = 0;
+        int l = 0;
+
+        for (int i = x - 2; i < x + 3; i++)
         {
-            for (int Jkesij = Jkesiy - 2; Jkesij < Jkesiy + 3; Jkesij++)
+            for (int j = y - 2; j < y + 3; j++)
             {
-                if (0 <= Jkesii && Jkesii < Paneru.GetLength(0) && 0 <= Jkesij && Jkesij < Paneru.GetLength(1))
+                if (0 <= i && i < Paneru.GetLength(0) && 0 <= j && j < Paneru.GetLength(1))
                 {
-                    if (Paneru[Jkesii, Jkesij] > 0)
+                    if (Paneru[i, j] > 0)
                     {
-                        Paneru[Jkesii, Jkesij] -= jyuuji[Jkesik, Jkesil];
+                        Paneru[i, j] -= kesi[k, l];
                     }
                 }
-                Jkesil++;
+                l++;
             }
-            Jkesil = 0;
-            Jkesik++;
+            l = 0;
+            k++;
         }
-        Jkesik = 0;
-        Jkesil = 0;
-    }
-    void Kurosukesi(int Kkesix, int Kkesiy)
-    {
-        int Kkesik = 0;
-        int Kkesil = 0;
-
-        for (int Kkesii = Kkesix - 2; Kkesii < Kkesix + 3; Kkesii++)
-        {
-            for (int Kkesij = Kkesiy - 2; Kkesij < Kkesiy + 3; Kkesij++)
-            {
-                if (0 <= Kkesii && Kkesii < Paneru.GetLength(0) && 0 <= Kkesij && Kkesij < Paneru.GetLength(1))
-                {
-                    if (Paneru[Kkesii, Kkesij] > 0)
-                    {
-                        Paneru[Kkesii, Kkesij] -= kurosu[Kkesik, Kkesil];
-                    }
-                }
-                Kkesil++;
-            }
-            Kkesil = 0;
-            Kkesik++;
-        }
-        Kkesik = 0;
-        Kkesil = 0;
-    }
-    void Rasenkesi(int Rkesix, int Rkesiy)
-    {
-        int Rkesik = 0;
-        int Rkesil = 0;
-
-        for (int Rkesii = Rkesix - 2; Rkesii < Rkesix + 3; Rkesii++)
-        {
-            for (int Rkesij = Rkesiy - 2; Rkesij < Rkesiy + 3; Rkesij++)
-            {
-                if (0 <= Rkesii && Rkesii < Paneru.GetLength(0) && 0 <= Rkesij && Rkesij < Paneru.GetLength(1))
-                {
-                    if (Paneru[Rkesii, Rkesij] > 0)
-                    {
-                        Paneru[Rkesii, Rkesij] -= rasen[Rkesik, Rkesil];
-                    }
-                }
-                Rkesil++;
-            }
-            Rkesil = 0;
-            Rkesik++;
-        }
-        Rkesik = 0;
-        Rkesil = 0;
+        k = 0;
+        l = 0;
     }
 
     //ボタン処理
@@ -501,220 +455,122 @@ public class PaneruController : MonoBehaviour
     {
         if (this.isJyuujikesiButton == true)
         {
-            Jyuujikesi(x, y);
+            Kesi(x, y,jyuuji);
             this.isJyuujikesiButton = false;
         }
         else if (this.isKurosukesiButton == true)
         {
-            Kurosukesi(x, y);
+            Kesi(x, y, kurosu);
             this.isKurosukesiButton = false;
         }
         else if (this.isRasenkesiButton == true)
         {
-            Rasenkesi(x, y);
+            Kesi(x, y, rasen);
             this.isRasenkesiButton = false;
         }
     }
 
     //Button00を押したら消し方に応じた(0,0)中心の処理をする
     public void GetButton00Down()
-    {
-        Buttonsyori(0, 0);
-    }
+    {Buttonsyori(0, 0);}
     public void GetButton01Down()
-    {
-        Buttonsyori(0, 1);
-    }
+    {Buttonsyori(0, 1);}
     public void GetButton02Down()
-    {
-        Buttonsyori(0, 2);
-    }
+    {Buttonsyori(0, 2);}
     public void GetButton03Down()
-    {
-        Buttonsyori(0, 3);
-    }
+    {Buttonsyori(0, 3);}
     public void GetButton04Down()
-    {
-        Buttonsyori(0, 4);
-    }
+    {Buttonsyori(0, 4);}
     public void GetButton05Down()
-    {
-        Buttonsyori(0, 5);
-    }
+    {Buttonsyori(0, 5);}
     public void GetButton06Down()
-    {
-        Buttonsyori(0, 6);
-    }
+    {Buttonsyori(0, 6);}
     public void GetButton10Down()
-    {
-        Buttonsyori(1, 0);
-    }
+    {Buttonsyori(1, 0);}
     public void GetButton11Down()
-    {
-        Buttonsyori(1, 1);
-    }
+    {Buttonsyori(1, 1);}
     public void GetButton12Down()
-    {
-        Buttonsyori(1, 2);
-    }
+    {Buttonsyori(1, 2);}
     public void GetButton13Down()
-    {
-        Buttonsyori(1, 3);
-    }
+    {Buttonsyori(1, 3);}
     public void GetButton14Down()
-    {
-        Buttonsyori(1, 4);
-    }
+    {Buttonsyori(1, 4);}
     public void GetButton15Down()
-    {
-        Buttonsyori(1, 5);
-    }
+    { Buttonsyori(1, 5); }
     public void GetButton16Down()
-    {
-        Buttonsyori(1, 6);
-    }
+    {Buttonsyori(1, 6);}
     public void GetButton20Down()
-    {
-        Buttonsyori(2, 0);
-    }
+    {Buttonsyori(2, 0);}
     public void GetButton21Down()
-    {
-        Buttonsyori(2, 1);
-    }
+    {Buttonsyori(2, 1);}
     public void GetButton22Down()
-    {
-        Buttonsyori(2, 2);
-    }
+    {Buttonsyori(2, 2);}
     public void GetButton23Down()
-    {
-        Buttonsyori(2, 3);
-    }
+    {Buttonsyori(2, 3);}
     public void GetButton24Down()
-    {
-        Buttonsyori(2, 4);
-    }
+    {Buttonsyori(2, 4);}
     public void GetButton25Down()
-    {
-        Buttonsyori(2, 5);
-    }
+    {Buttonsyori(2, 5);}
     public void GetButton26Down()
-    {
-        Buttonsyori(2, 6);
-    }
+    {Buttonsyori(2, 6);}
     public void GetButton30Down()
-    {
-        Buttonsyori(3, 0);
-    }
+    {Buttonsyori(3, 0);}
     public void GetButton31Down()
-    {
-        Buttonsyori(3, 1);
-    }
+    {Buttonsyori(3, 1);}
     public void GetButton32Down()
-    {
-        Buttonsyori(3, 2);
-    }
+    { Buttonsyori(3, 2); }
     public void GetButton33Down()
-    {
-        Buttonsyori(3, 3);
-    }
+    {Buttonsyori(3, 3);}
     public void GetButton34Down()
-    {
-        Buttonsyori(3, 4);
-    }
+    {Buttonsyori(3, 4);}
     public void GetButton35Down()
-    {
-        Buttonsyori(3, 5);
-    }
+    {Buttonsyori(3, 5);}
     public void GetButton36Down()
-    {
-        Buttonsyori(3, 6);
-    }
+    {Buttonsyori(3, 6);}
     public void GetButton40Down()
-    {
-        Buttonsyori(4, 0);
-    }
+    {Buttonsyori(4, 0);}
     public void GetButton41Down()
-    {
-        Buttonsyori(4, 1);
-    }
+    {Buttonsyori(4, 1);}
     public void GetButton42Down()
-    {
-        Buttonsyori(4, 2);
-    }
+    {Buttonsyori(4, 2);}
     public void GetButton43Down()
-    {
-        Buttonsyori(4, 3);
-    }
+    {Buttonsyori(4, 3);}
     public void GetButton44Down()
-    {
-        Buttonsyori(4, 4);
-    }
+    {Buttonsyori(4, 4);}
     public void GetButton45Down()
-    {
-        Buttonsyori(4, 5);
-    }
+    {Buttonsyori(4, 5);}
     public void GetButton46Down()
-    {
-        Buttonsyori(4, 6);
-    }
+    { Buttonsyori(4, 6);}
     public void GetButton50Down()
-    {
-        Buttonsyori(5, 0);
-    }
+    {Buttonsyori(5, 0);}
     public void GetButton51Down()
-    {
-        Buttonsyori(5, 1);
-    }
+    {Buttonsyori(5, 1);}
     public void GetButton52Down()
-    {
-        Buttonsyori(5, 2);
-    }
+    {Buttonsyori(5, 2);}
     public void GetButton53Down()
-    {
-        Buttonsyori(5, 3);
-    }
+    {Buttonsyori(5, 3);}
     public void GetButton54Down()
-    {
-        Buttonsyori(5, 4);
-    }
+    {Buttonsyori(5, 4);}
     public void GetButton55Down()
-    {
-        Buttonsyori(5, 5);
-    }
+    {Buttonsyori(5, 5);}
     public void GetButton56Down()
-    {
-        Buttonsyori(5, 6);
-    }
+    {Buttonsyori(5, 6);}
     public void GetButton60Down()
-    {
-        Buttonsyori(6, 0);
-    }
+    {Buttonsyori(6, 0);}
     public void GetButton61Down()
-    {
-        Buttonsyori(6, 1);
-    }
+    {Buttonsyori(6, 1);}
     public void GetButton62Down()
-    {
-        Buttonsyori(6, 2);
-    }
+    {Buttonsyori(6, 2);}
     public void GetButton63Down()
-    {
-        Buttonsyori(6, 3);
-    }
+    {Buttonsyori(6, 3);}
     public void GetButton64Down()
-    {
-        Buttonsyori(6, 4);
-    }
+    {Buttonsyori(6, 4);}
     public void GetButton65Down()
-    {
-        Buttonsyori(6, 5);
-    }
+    {Buttonsyori(6, 5);}
     public void GetButton66Down()
-    {
-        Buttonsyori(6, 6);
-    }
+    {Buttonsyori(6, 6);}
 
-    //ボタンの色変更
+    //パネルの色変更
     void ColorChange(GameObject color, int colorx)
     {
         if(colorx==0)
